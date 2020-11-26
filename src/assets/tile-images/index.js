21ecb46f7509af92988ec47cb6039995fbc8fa87
Dan Shields <d.shieldsuk@gmail.com>
2020-11-26 17:17:14 +0000

    feat: add images

---
+++ src/assets/tile-images/index.js
@@ -0,0 +1,12 @@
+import essentialstf from './essentialstf.svg';
+import faceit from './faceit.svg';
+import github from './github.png';
+import linkedin from './linkedin.png';
+import twitter from './twitter.svg';
+import uomesports from './uomesports.png';
+import nodecg from './nodecg.png';
+import at from './at.png';
+import kotn from './kotn.png';
+import hiveaid from './hiveaid.png';
+
+export default { essentialstf, faceit, github, linkedin, twitter, uomesports, nodecg, at, hiveaid, kotn };
