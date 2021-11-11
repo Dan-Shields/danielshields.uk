62d7eba2259d5f10897b35256713f46c0614ab80
Dan Shields <d.shieldsuk@gmail.com>
2021-11-11 15:25:29 +0000

    chore: update lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,37 @@
 module.exports = {
-    root: true,
+    env: {
+        browser: true,
+        es6: true
+    },
     extends: [
         'plugin:vue/vue3-recommended',
-        'eslint:recommended',
         '@vue/typescript'
     ],
-    env: {
-        "browser": true,
-        "amd": true,
-        "node": true
-    },
     rules: {
-        '@typescript-eslint/indent': [2, 4, {'SwitchCase': 1}],
-        'vue/html-indent': [2, 4]
+        'semi': ["error", "never"],
+        'no-console': 0,
+        'no-debugger': 0,
+        'no-use-before-define': [2, { 'functions': false }],
+        'padded-blocks': 0,
+        'no-undef': 0,
+        'no-constant-condition': ['error', { 'checkLoops': false }],
+        'constructor-super': 0,
+
+        'vue/v-bind-style': 1,
+        'vue/v-on-style': 1,
+        'vue/no-v-html': 1,
+        'vue/html-self-closing': [2, {
+            'html': {
+                'component': 'always',
+                'normal': 'never'
+            }
+        }],
+        'vue/attribute-hyphenation': 0,
+        'vue/html-indent': [2, 4],
+        'vue/max-attributes-per-line': [1, {
+            'singleline': 3
+        }],
+        'vue/singleline-html-element-content-newline': 0
     },
     ignorePatterns: [
         'dist/*',
