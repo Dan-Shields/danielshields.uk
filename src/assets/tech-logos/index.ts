fd1ac62ab4a4b7216b87a1b523fcdd3d9b9803c6
Daniel Shields <me@danielshields.uk>
2022-09-27 21:37:49 +0100

    compress most images to webp

--- src/assets/tech-logos/index.ts
+++ src/assets/tech-logos/index.ts
@@ -1,7 +1,10 @@
 import bmd from './bmd-logo-small.png'
 import xpression from './xpression.png'
 import obs from './obs.png'
-import vmix from './vMix-Logo-Black.png'
 import casparcg from './casparlogo.png'
+import js from './js-ts.jpg'
+import ue4 from './ue4.webp'
+import csharp from './csharp.svg'
+import vue from './vue.png'
 
-export default { bmd, xpression, obs, vmix, casparcg }
+export default { bmd, xpression, obs, casparcg, js, ue4, csharp, vue }
